92a7be493288cc4958b663561e879ddf3525f36c
Katarina Koderman <koderman.katarina@gmail.com>
2017-04-10 00:19:44 +0200

    Ustvarila razred človek.

---
+++ clovek.py
@@ -0,0 +1,19 @@
+######################################################################
+## Igralec človek
+
+class Clovek():
+    def __init__(self, gui):
+        self.gui = gui
+
+    def igraj(self):
+        # Na potezi je uporabnik. Čakamo, da bo kliknil na ploščo.
+        # Ko se bo to zgodilo, nas bo Gui obvestil preko metode klik.
+        pass
+
+    def prekini(self):
+        # Metoda, ki jo kliče gui, če je treba prekiniti razmišljanje.
+        pass
+
+    def klik(self, p):
+        # Povlečemo potezo. Če ni veljavna, se ne zgodi nič.
+        self.gui.povleci_potezo(p)
