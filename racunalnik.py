51888c7f6dea9452613f2aec5bc54ba917239bee
Katarina Koderman <koderman.katarina@gmail.com>
2017-05-03 21:50:48 +0200

    Uredila razred.

--- racunalnik.py
+++ racunalnik.py
@@ -1,3 +1,9 @@
+#########################################################################
+#                                                                       #
+#                           Igralec računalnik                          #
+#                                                                       #
+#########################################################################
+
 import threading
 import minimax
 import logging
@@ -10,7 +16,7 @@ class Racunalnik():
 
     def igraj(self):
         """Igraj potezo, ki jo vrne algoritem."""
-        # Naredimo vlakno, ki mu podamo kopijo igre (da ne zmede gui-ja):
+        # Naredimo vlakno, ki mu podamo kopijo igre (da ne zmede gui-ja).
         self.mislec = threading.Thread(
             target=lambda: self.algoritem.izracunaj_potezo(self.gui.logika.kopija()))
 
@@ -21,26 +27,26 @@ class Racunalnik():
         self.gui.plosca.after(100, self.preveri_potezo)
 
     def preveri_potezo(self):
-        """Vsakih 100ms preveri, ali je algoritem že izračunal potezo."""
+        """Vsakih 100ms preveri, če je algoritem že izračunal potezo."""
         if self.algoritem.poteza is not None:
-            # Algoritem je našel potezo, povleci jo, če ni bilo prekinitve
+            # Algoritem je našel potezo, če ni bilo prekinitve, jo naredi.
             self.gui.naredi_potezo(self.algoritem.poteza)
-            # Vzporedno vlakno ni več aktivno, zato ga ne rabimo več
+            # Vzporedno vlakno ni več aktivno, zato ga ne rabimo več.
             self.mislec = None
         else:
-            # Algoritem še ni našel poteze, preveri še enkrat čez 100ms
+            # Algoritem še ni našel poteze, preveri še enkrat čez 100ms.
             self.gui.plosca.after(100, self.preveri_potezo)
 
     def prekini(self):
-        # To metodo kliče gui, če je treba prekiniti razmišljanje.
+        """Prekine razmišljanje računalnika."""
         if self.mislec:
             logging.debug("Prekinjamo {0}".format(self.mislec))
-            # Algoritmu sporočimo, da mora nehati z razmišljanjem
+            # Algoritmu sporočimo, da mora nehati z razmišljanjem.
             self.algoritem.prekini()
-            # Počakamo, da se vlakno ustavi
+            # Počakamo, da se vlakno ustavi.
             self.mislec.join()
             self.mislec = None
 
     def klik(self, p):
-        # Računalnik ignorira klike uporabnika
+        """Ignorira klike uporabnika med razmišljanjem računalnika."""
         pass
