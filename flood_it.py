7e2a6134cbb4bbccb65e7b7c128589d4e6f79c1d
Katarina Koderman <koderman.katarina@gmail.com>
2017-05-10 21:43:15 +0200

    Zadnji popravki.

--- flood_it.py
+++ flood_it.py
@@ -10,24 +10,25 @@ import tkinter    # za uporabniški vmesnik
 import argparse   # za argumente iz ukazne vrstice
 import logging    # za odpravljanje napak
 
-# Privzeta minimax globina
-MINIMAX_GLOBINA = 6
+# Privzeta minimax globina:
+MINIMAX_GLOBINA = 5  # Globino razmišljanja računalnika lahko spreminjamo. Smiselne so vrednosti med 3 in 7.
 
 import logika
 import clovek
 import racunalnik
 import minimax
 
-# TODO velikost polja se mora spremeniti, če se spremeni velikost okna
-
 # Uvedemo parameter:
-VELIKOST_IGRALNE_PLOSCE = 12
+VELIKOST_IGRALNE_PLOSCE = 12  # Velikost igralne plošče lahko spreminjamo. Smiselne so vrednosti med 5 in 20.
 
 
 class Gui():
     # Vpeljemo konstante:
     VELIKOST_POLJA = 1
+    # Seznam barv gumbov in polj:
     SEZNAM_BARV = ['deep sky blue', 'yellow', 'snow4', 'lawn green', 'maroon1', 'navy']
+    # Seznam barv lahko  spreminjamo, vendar se mora ohraniti dolžina seznama.
+    # Izbrane barve morajo biti različne.
 
     def __init__(self, master, globina):
         self.logika = None  # Tu bo spravljena logika igre, ko se bo igra dejansko začela.
@@ -50,30 +51,30 @@ class Gui():
         self.seznam_gumbov = []
         for i in range(len(Gui.SEZNAM_BARV)):
             gumb_v_nastajanju = tkinter.Button(gumbi, width=5 * Gui.VELIKOST_POLJA, height=2 * Gui.VELIKOST_POLJA,
-                           text=Gui.SEZNAM_BARV[i],
+                           text=" ", highlightbackground=Gui.SEZNAM_BARV[i],
                            background=Gui.SEZNAM_BARV[i], command=lambda i=i: self.barva_klik(i))
             gumb_v_nastajanju.pack(side=tkinter.LEFT, padx=10, pady=5)
             self.seznam_gumbov.append(gumb_v_nastajanju)
 
         # Levo in desno od igralne plosce postavimo label-a z vmesnim rezultatom.
-        levi_okvir = tkinter.Frame(master)  # okvir v katerem sta ime Igralca 1 in njegov rezultat leva_vrednost
+        levi_okvir = tkinter.Frame(master)  # Okvir v katerem sta ime Igralca 1 in njegov rezultat leva_vrednost.
         levi_okvir.grid(row=1, column=0, padx=20, sticky="S")
         self.leva_vrednost = tkinter.Label(levi_okvir, text="0", font=("Comic Sans", 16), borderwidth=20)
         self.leva_vrednost.pack(side=tkinter.BOTTOM)
 
-        desni_okvir = tkinter.Frame(master)  # okvir v katerem sta ime Igralca 2 in njegov rezultat desna_vrednost
+        desni_okvir = tkinter.Frame(master)  # Okvir v katerem sta ime Igralca 2 in njegov rezultat desna_vrednost.
         desni_okvir.grid(row=1, column=2, padx=20, sticky="S")
         self.desna_vrednost = tkinter.Label(desni_okvir, text="0", font=("Comic Sans", 16), borderwidth=20)
         self.desna_vrednost.pack(side=tkinter.BOTTOM)
 
         def omejitev_stevila_znakov(event):
-            """Dopušča vnos v Entry do dolžine 15, daljše nize skrajša na to dolžino."""
-            if len(self.ime_igralca1.get()) > 15:
-                skrajsano_ime = self.ime_igralca1.get()[:15]
+            """Dopušča vnos v Entry do dolžine 14, daljše nize skrajša na to dolžino."""
+            if len(self.ime_igralca1.get()) > 14:
+                skrajsano_ime = self.ime_igralca1.get()[:14]
                 self.ime_igralca1.delete(0, len(self.ime_igralca1.get()))
                 self.ime_igralca1.insert(0, skrajsano_ime)
-            elif len(self.ime_igralca2.get()) > 15:
-                skrajsano_ime = self.ime_igralca2.get()[:15]
+            elif len(self.ime_igralca2.get()) > 14:
+                skrajsano_ime = self.ime_igralca2.get()[:14]
                 self.ime_igralca2.delete(0, len(self.ime_igralca2.get()))
                 self.ime_igralca2.insert(0, skrajsano_ime)
 
@@ -95,7 +96,6 @@ class Gui():
         # Okvir za igralno polje:
         self.plosca = tkinter.Frame(master)
         self.plosca.grid(row=1, column=1, rowspan=2)
-
 
 
         # Naredimo glavni menu:
@@ -192,11 +192,11 @@ class Gui():
                 defaultbg = root.cget("bg")
                 self.seznam_gumbov[barva].config(state="disabled", background=defaultbg)
             else:
-                # Gumb izključimo.
-                defaultbg = root.cget("bg")
+                # Gumb vključimo.
                 self.seznam_gumbov[barva].config(state="normal", background="{}".format(Gui.SEZNAM_BARV[barva]))
         self.igralec1.igraj()
-        # Omogočimo gumbe za razveljavitev potez
+
+        # Omogočimo gumbe za razveljavitev potez:
         self.moznosti.entryconfig(1, state=tkinter.NORMAL)
         self.moznosti.entryconfig(0, state=tkinter.NORMAL)
 
@@ -237,23 +237,25 @@ class Gui():
         """Nastavi stanje igre na konec igre. Uporabniku onemogoči razveljavitev igre.
         V vrstico z opozorili izpiše zmagovalca."""
 
-        self.moznosti.entryconfig(1, state=tkinter.DISABLED) # Onemogočimo gumbe za razveljavlitev
+        # Onemogočimo gumbe za razveljavitev:
+        self.moznosti.entryconfig(1, state=tkinter.DISABLED)
         self.moznosti.entryconfig(0, state=tkinter.DISABLED)
+
         (levi_rezultat, desni_rezultat) = self.logika.get_rezultat()
         if zmagovalec == logika.IGRALEC1:
-            if levi_rezultat + desni_rezultat != VELIKOST_IGRALNE_PLOSCE**2:
-                self.opozorila.config(text="Porabila sta vse poteze. Zmagal je igralec {}.".format(self.ime_igralca1.get()))
+            if levi_rezultat + desni_rezultat != VELIKOST_IGRALNE_PLOSCE ** 2:
+                self.opozorila.config(text="Ni več potez. Bravo, {}!".format(self.ime_igralca1.get()))
             else:
                 self.opozorila.config(text="Bravo {}! Zmaga je tvoja!".format(self.ime_igralca1.get()))
                 self.leva_vrednost.config(font=("Comic Sans", 20, "bold"))
         elif zmagovalec == logika.IGRALEC2:
-            if levi_rezultat + desni_rezultat != VELIKOST_IGRALNE_PLOSCE**2:
-                self.opozorila.config(text="Porabila sta vse poteze. Zmagal je igralec {}.".format(self.ime_igralca2.get()))
+            if levi_rezultat + desni_rezultat != VELIKOST_IGRALNE_PLOSCE ** 2:
+                self.opozorila.config(text="Ni več potez. Bravo, {}!".format(self.ime_igralca2.get()))
             else:
                 self.opozorila.config(text="Bravo {}! Zmaga je tvoja!".format(self.ime_igralca2.get()))
                 self.desna_vrednost.config(font=("Comic Sans", 20, "bold"))
         else:
-            if levi_rezultat + desni_rezultat != VELIKOST_IGRALNE_PLOSCE**2:
+            if levi_rezultat + desni_rezultat != VELIKOST_IGRALNE_PLOSCE ** 2:
                 self.opozorila.config(text="Porabila sta vse poteze. Rezultat je neodločen.")
             else:
                 self.opozorila.config(text="Igre je konec, rezultat pa neodločen.")
@@ -262,7 +264,7 @@ class Gui():
 
     def prekini_igralce(self):
         """Igralcem sporoči, da nehajo razmišljati."""
-        logging.debug("prekinjam igralce")
+        logging.debug("Prekinjam igralce.")
         if self.igralec1:
             self.igralec1.prekini()
         if self.igralec2:
@@ -276,10 +278,12 @@ class Gui():
 
 #########################################################################
 
-    def razveljavi_eno_potezo(self):# Uporabno pri igri proti človeku.
-        """Vrne ploščo na stanje pred zadnjo potezo."""
+    # Metodi za razveljavljanje potez:
+    def razveljavi_eno_potezo(self):  # Uporabno pri igri proti človeku.
+        """Vrne igralno ploščo na stanje pred zadnjo potezo."""
         self.prekini_igralce()
-        (pozicija, na_potezi) = self.logika.razveljavi() # Iz zgodovine dobimo zadnjo pozicijo in kdo je bil takrat na potezi
+        # Iz zgodovine dobimo zadnjo pozicijo in kdo je bil takrat na potezi:
+        (pozicija, na_potezi) = self.logika.razveljavi()
         self.matrika = pozicija
         if na_potezi == "IGRALEC1":
             self.igralec1.igraj()
@@ -287,18 +291,18 @@ class Gui():
             self.igralec2.igraj()
         self.posodobi()
 
-    def razveljavi_dve_potezi(self): # Uporabno pri igri proti računalniku
+    def razveljavi_dve_potezi(self):  # Uporabno pri igri proti računalniku.
         """Vrne ploščo pred dvema potezama."""
         self.prekini_igralce()
         self.logika.razveljavi()
-        (pozicija, na_potezi) = self.logika.razveljavi() # Iz zgodovine dobimo predzadnjo pozicijo in kdo je bil takrat na potezi
+        # Iz zgodovine dobimo predzadnjo pozicijo in kdo je bil takrat na potezi:
+        (pozicija, na_potezi) = self.logika.razveljavi()
         self.matrika = pozicija
         if na_potezi == "IGRALEC1":
             self.igralec1.igraj()
         else:
             self.igralec2.igraj()
         self.posodobi()
-
 
 #########################################################################
 #########################################################################
@@ -330,11 +334,10 @@ if __name__ == "__main__":
 
     # Naredimo glavno okno in nastavimo ime:
     root = tkinter.Tk()
-    root.title("Color Flood")
+    root.title("Color Wars")
 
     # Naredimo objekt razreda Gui in ga spravimo v spremenljivko.
-    globina = args.globina
-    aplikacija = Gui(root, globina)
+    aplikacija = Gui(root, args.globina)
 
     # Kontrolo prepustimo glavnemu oknu. Funkcija mainloop neha delovati, ko okno zapremo.
     root.mainloop()
